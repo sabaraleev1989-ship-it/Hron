8adc92385949cd44c1765268454ef9ac0510448a
sabaraleev1989-ship-it <sabaraleev1989@gmail.com>
2026-04-22 16:51:09 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'scorpion-v6';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
